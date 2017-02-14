7031a243fb64566687a2ed18149c658bc400304d
Barry Joyce <barryjohnjoyce@gmail.com>
2017-02-14 16:09:15 +0000

    settings error check 2

--- Stream3Project/settings.py
+++ Stream3Project/settings.py
@@ -188,13 +188,12 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"), #commented out for AWS implementation
 )
 
-
-# STATICFILES_LOCATION = 'static'
+STATICFILES_LOCATION = 'static'
 # STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 # STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 #
 
-# MEDIAFILES_LOCATION = 'media'
+MEDIAFILES_LOCATION = 'media'
 # DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 # MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
