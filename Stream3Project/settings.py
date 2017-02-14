440ad6325aa6fe98c359816e054287de866a2305
Barry Joyce <barryjohnjoyce@gmail.com>
2017-02-14 18:50:34 +0000

    static settings amended to recheck AWS deployment

--- Stream3Project/settings.py
+++ Stream3Project/settings.py
@@ -48,10 +48,7 @@ INSTALLED_APPS = [
     'threadsApp',
     'pollsApp',
     'django_gravatar',
-
     'storages',
-
-
 ]
 
 MIDDLEWARE = [
@@ -90,8 +87,6 @@ WSGI_APPLICATION = 'Stream3Project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-#
 
 DATABASES = {
     'default': {
@@ -173,35 +168,35 @@ ALLOWED_HOSTS = ['counterpoint2020.herokuapp.com', '127.0.0.1']
 #     'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
 #     'Cache-Control': 'max-age=94608000',
 # }
-#
-# AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-# AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-# AWS_S3_HOST = 's3-eu-west-1.amazonaws.com'
+
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_HOST = 's3-eu-west-1.amazonaws.com'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/' #comment out for AWS implementation
+# STATIC_URL = '/static/' #comment out for AWS implementation
 STATIC_ROOT = ''
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"), #comment out for AWS implementation
 )
 
-# STATICFILES_LOCATION = 'static'
-# STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-# STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-#
-#
-# MEDIAFILES_LOCATION = 'media'
-# DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-# MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
 #
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #commented out for AWS implementation
-MEDIA_URL = '/media/' #commented out for AWS implementation
+# MEDIA_URL = '/media/' #commented out for AWS implementation
 
 
 
